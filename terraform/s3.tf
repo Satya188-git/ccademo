d75a8589e9c54326d2903139be568238ebeb0054
Racharla <saipraneeth.racharla@accenture.com>
2024-09-20 13:23:03 +0530

    Added athena results bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,4 +1,4 @@
-module "s3_bucket" {
+module "s3_bucket_athena_results" {
   source  = "app.terraform.io/SempraUtilities/seu-s3/aws"
   version = "11.1.3"
 
@@ -6,11 +6,29 @@ module "s3_bucket" {
   application_code         = var.application_code
   environment_code         = var.environment_code
   region_code              = var.region_code
-  application_use          = "${var.application_use}-sai-tf-testing"
+  application_use          = "${var.application_use}-athena-results"
   create_bucket            = true
-  versioning               = true
+  versioning               = False
   object_ownership         = "BucketOwnerPreferred"
   control_object_ownership = true
   tags                     = var.tags
 }
+
+module "s3_bucket_nice" {
+  source  = "app.terraform.io/SempraUtilities/seu-s3/aws"
+  version = "11.1.3"
+
+  company_code             = var.company_code
+  application_code         = var.application_code
+  environment_code         = var.environment_code
+  region_code              = var.region_code
+  application_use          = "${var.application_use}-nice-data"
+  create_bucket            = true
+  versioning               = False
+  object_ownership         = "BucketOwnerPreferred"
+  control_object_ownership = true
+  tags                     = var.tags
+}
+
+
 #test
