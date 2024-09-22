7adccb1e2426f73223126a3e00c32264edf50a79
Racharla <saipraneeth.racharla@accenture.com>
2024-09-22 09:42:09 +0530

    Updated policy json

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -10,7 +10,8 @@ module "lambda_role" {
   description       = "This is a lambda role to write data to S3 and update glue crawlers"
   service_resources = ["lambda.amazonaws.com"]
   tags              = var.tags
-  additional_policy_statements = [{
+  additional_policy_statements = [
+		{
 			"Effect": "Allow",
 			"Action": [
 				"glue:GetCrawler",
@@ -23,47 +24,6 @@ module "lambda_role" {
 				"arn:aws:glue:us-west-2:${var.awsAccount}:database/*",
 				"arn:aws:glue:us-west-2:${var.awsAccount}:crawler/*"
 			]
-		},
-		{
-			"Effect": "Allow",
-			"Action": [
-				"s3:ReplicateObject",
-				"s3:PutObject",
-				"s3:GetObject",
-				"s3:GetObjectAttributes",
-				"s3:ListBucket",
-				"s3:DeleteObject",
-				"s3:GetBucketLocation",
-				"s3:ListMultipartUploadParts"
-			],
-			"Resource": [
-				"arn:aws:s3:::${var.company_code}-${var.application_code}-${var.environment_code}-${var.region_code}-s3-${var.application_use}",
-				"*"
-			]
-		},
-		{
-			"Effect": "Allow",
-			"Action": [
-				"athena:ListDatabases",
-				"athena:ListDataCatalogs",
-				"athena:GetTable",
-				"athena:GetTableMetadata",
-				"athena:GetTables",
-				"athena:RunQuery"
-			],
-			"Resource": [
-				"*"
-			]
-		},
-		{
-			"Effect": "Allow",
-			"Action": [
-				"logs:CreateLogGroup",
-				"logs:CreateLogStream",
-				"logs:PutLogEvents"
-			],
-			"Resource": [
-				"*"
-			]
-		}]
+		}
+	]
 }
