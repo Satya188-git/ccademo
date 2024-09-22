a497bba54622264bf567cc737b958fe95e7f69c5
Racharla <saipraneeth.racharla@accenture.com>
2024-09-22 23:31:23 +0530

    Added variables

--- terraform/secretsmanager.tf
+++ terraform/secretsmanager.tf
@@ -8,7 +8,7 @@ resource "aws_secretsmanager_secret" "nice_secrets_manager" {
 resource "aws_secretsmanager_secret_version" "nice_secrets" {
   secret_id     = aws_secretsmanager_secret.nice_secrets_manager.id
   secret_string = jsonencode({
-    nice_common_api_key = var.TF_VAR_nice_api_key
-    nice_hist_queuestats_key = var.TF_VAR_nice_api_secret
+    nice_common_api_key = var.nice_api_key
+    nice_hist_queuestats_key = var.nice_api_secret
   })
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -51,12 +51,12 @@ variable "kms_key_id" {
   type        = string
 }
 
-variable "TF_VAR_nice_api_key" {
+variable "nice_api_key" {
   description = "Nice API Key from ADO"
   type        = string
 }
 
-variable "TF_VAR_nice_api_secret" {
+variable "nice_api_secret" {
   description = "Nice API Key from ADO"
   type        = string
 }
