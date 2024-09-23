4ff8f1b5d52415ef53f55664c6112ded0d4f13f5
Souvik Nayak <souvik.nath.nayak@accenture.com>
2024-09-23 22:29:10 +0530

    Adding the lake formation tables and database

--- terraform/lake_formation.tf
+++ terraform/lake_formation.tf
@@ -14,7 +14,7 @@ module "lake-formation-nice" {
 
   assign_iam_admin    = true
   iam_admin_role_arn  = [var.admins_arn , devs_arn]
-  iam_admin_role_name = [AWSReservedSSO_sdge-dcctr-dev-admin_f4611a12900c932f, AWSReservedSSO_sdge-dcctr-dev-developer_e540a5b0e1ae0e8f]
+  iam_admin_role_name = ["AWSReservedSSO_sdge-dcctr-dev-admin_f4611a12900c932f", "AWSReservedSSO_sdge-dcctr-dev-developer_e540a5b0e1ae0e8f"]
 
 }
 
