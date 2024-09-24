638d7000129164013768144432a304aa952d0dd8
Racharla <saipraneeth.racharla@accenture.com>
2024-09-24 16:30:17 +0530

    Added iam admin permissions for LF

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,12 +1,12 @@
 module "lakeformation_admin" {
   source  = "app.terraform.io/SempraUtilities/seu-iam-role/aws"
   version = "10.0.2"
-  company_code     = var.company_code
-  application_code = var.application_code
-  environment_code = var.environment_code
-  region_code      = var.region_code
-  application_use  = var.application_use
-  description      = "IAM role for Lake Formation"
-  service_resource = "glue.amazonaws.com"
-  tags             = var.tags
+  company_code      = var.company_code
+  application_code  = var.application_code
+  environment_code  = var.environment_code
+  region_code       = var.region_code
+  application_use   = var.application_use
+  description       = "IAM role for Lake Formation"
+  service_resources = "glue.amazonaws.com"
+  tags              = var.tags
 }
