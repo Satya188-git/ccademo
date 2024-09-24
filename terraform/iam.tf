e2873e9ebaeec7cd74662ada9b94fde4677efa52
Racharla <saipraneeth.racharla@accenture.com>
2024-09-24 19:01:36 +0530

    Added GDC Table config

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -10,3 +10,34 @@ module "lakeformation_admin" {
   service_resources = ["glue.amazonaws.com"]
   tags              = var.tags
 }
+
+module "lambda_role" {
+  source  = "app.terraform.io/SempraUtilities/seu-iam-role/aws"
+  version = "10.0.2"
+
+  company_code      = var.company_code
+  application_code  = var.application_code
+  environment_code  = var.environment_code
+  region_code       = var.region_code
+  application_use   = "${var.application_use}-lambda"
+  description       = "This is a lambda role to write data to S3 and update glue crawlers"
+  service_resources = ["lambda.amazonaws.com"]
+  tags              = var.tags
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name   = "${var.company_code}-${var.application_code}-${var.environment_code}-${var.region_code}-${var.application_use}-lambda-policy"
+  role   = module.lambda_role.name
+  policy = templatefile(
+    "${path.module}/iampolicies/policy-iam-lambda-assume-role.tmpl",
+    {
+      region = "us-west-2",
+      region_code = var.region_code
+      account = var.awsAccount,
+      company_code = var.company_code,
+      application_code = var.application_code,
+      environment_code = var.environment_code,
+      application_use = "${var.application_use}-nice-data"
+    }
+  )
+}
