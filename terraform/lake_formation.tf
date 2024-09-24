51b6dd4d826c04e0bcb4f7f9599166f012ade21b
Racharla <saipraneeth.racharla@accenture.com>
2024-09-24 18:02:39 +0530

    Added LF table creation Roles

--- terraform/lake_formation.tf
+++ terraform/lake_formation.tf
@@ -11,7 +11,11 @@ module "lake-formation-nice" {
   depends_on = [aws_glue_catalog_database.nice_glue_database, module.lakeformation_admin]
 
   set_glue_data_catalog_permissions = true
-  # trusted_resource_owners           = var.trusted_resource_owners_id
+  trusted_resource_owners           = [
+  "arn:aws:iam::442426866507:role/fondo/sdge-dcctr-dev-terraform-oidc-role",
+  "arn:aws:iam::442426866507:role/sdge-dcctr-dev-wus2-iam-role-ccc-analytics-lake-formation",
+  data.aws_iam_session_context.current.issuer_arn
+  ]
 
   assign_iam_admin    = true
 
