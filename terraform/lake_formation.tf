b14eb32edd76b05b21dbd953a0608d84498f240d
Racharla <saipraneeth.racharla@accenture.com>
2024-09-24 18:44:44 +0530

    Added GDC Table

--- terraform/lake_formation.tf
+++ terraform/lake_formation.tf
@@ -24,50 +24,50 @@ module "lake-formation-nice" {
 }
 
 
-resource "aws_glue_catalog_table" "glue_table_links" {
-  count = length(var.source_table_names)
+# resource "aws_glue_catalog_table" "glue_table_links" {
+#   count = length(var.source_table_names)
 
-  name          = "${element(var.source_table_names, count.index)}_link"
-  database_name = aws_glue_catalog_database.nice_glue_database.name
-  catalog_id    = var.catalog_id # AWS Account ID of the source catalog (external AWS account)
+#   name          = "${element(var.source_table_names, count.index)}_link"
+#   database_name = aws_glue_catalog_database.nice_glue_database.name
+#   catalog_id    = var.catalog_id # AWS Account ID of the source catalog (external AWS account)
 
-  table_type = "GOVERNED"  # This is important for resource links
-  parameters = {
-    "targetTable"     = jsonencode({
-      "CatalogId"     = "632182196722"
-      "DatabaseName"  = "connect_datalake"
-      "Name"          = element(var.source_table_names, count.index)
-    })
-  }
+#   table_type = "GOVERNED"  # This is important for resource links
+#   parameters = {
+#     "targetTable"     = jsonencode({
+#       "CatalogId"     = "632182196722"
+#       "DatabaseName"  = "connect_datalake"
+#       "Name"          = element(var.source_table_names, count.index)
+#     })
+#   }
 
-  # Depends on the database creation
-  depends_on = [aws_glue_catalog_database.nice_glue_database, module.lakeformation_admin]
-}
+#   # Depends on the database creation
+#   depends_on = [aws_glue_catalog_database.nice_glue_database, module.lakeformation_admin]
+# }
 
-# module "gdc_table" {
-#   source  = "app.terraform.io/SempraUtilities/seu-glue-data-catalog/aws"
-#   version = "10.0.4"
-#   company_code      = var.company_code
-#   application_code  = var.application_code
-#   environment_code  = var.environment_code
-#   region_code       = var.region_code
-#   application_use   = "${var.application_use}-lf-gdc"
-#   tags = var.tags
-#   # glue catalog database
-#   glue_database_name = "analytics_database"
+module "gdc_table" {
+  source  = "app.terraform.io/SempraUtilities/seu-glue-data-catalog/aws"
+  version = "10.0.4"
+  company_code      = var.company_code
+  application_code  = var.application_code
+  environment_code  = var.environment_code
+  region_code       = var.region_code
+  application_use   = "${var.application_use}-lf-gdc"
+  tags = var.tags
+  # glue catalog database
+  glue_database_name = "analytics_database"
 
-#   add_linked_database = true
-#   target_catalog_id = "632182196722"
-#   target_database_name = "connect_datalake"
+  add_linked_database = true
+  target_catalog_id = "632182196722"
+  target_database_name = "connect_datalake"
 
-#   glue_catalog_map = {
-#     "sample_table_1" = {
-#       name                           = "sample_table_1"
-#       glue_catalog_table_description = "Table created using LF in GDC"
-#       # glue_catalog_table_table_type  = local.glue_catalog_table_table_type
+  glue_catalog_map = {
+    "sample_table_1" = {
+      name                           = "sample_table_1"
+      glue_catalog_table_description = "Table created using LF in GDC"
+      # glue_catalog_table_table_type  = local.glue_catalog_table_table_type
 
-#     }
+    }
 
     
-#   }
-# }
+  }
+}
