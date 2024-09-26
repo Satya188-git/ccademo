88ce7186249ee12b628d21ae3cb9f14ee7f013fe
Saipraneeth Racharla <SRacharl@sdgecontractor.com>
2024-09-26 18:40:48 +0530

    added count index

--- terraform/lake_formation.tf
+++ terraform/lake_formation.tf
@@ -176,7 +176,7 @@ module "glue_data_catalog" {
 resource "aws_lakeformation_permissions" "database" {
   count       = length(var.quicksight_user_arns)
   depends_on = [module.glue_data_catalog]
-  principal                     = element(var.quicksight_user_arns)
+  principal                     = element(var.quicksight_user_arns, count.index )
   permissions                   = ["DESCRIBE"]
   permissions_with_grant_option = ["DESCRIBE"]
   database {
