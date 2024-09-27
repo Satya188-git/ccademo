3a565ea1fb22d83aa1eb27b0d727052e73a10c7a
Saipraneeth Racharla <SRacharl@sdgecontractor.com>
2024-09-27 17:29:04 +0530

    Added seperate glue databases and lake formation pemissions using existing module.

---
+++ terraform/glue_catalog.tf
@@ -0,0 +1,56 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_session_context" "current" {
+  arn = data.aws_caller_identity.current.arn
+}
+
+
+# Module to create connect data resource link in Lake formation
+module "glue_data_catalog_connect_datalake" {
+  source  = "app.terraform.io/SempraUtilities/seu-glue-data-catalog/aws"
+  version = "10.0.4"
+  company_code      = var.company_code
+  application_code  = var.application_code
+  environment_code  = var.environment_code
+  region_code       = var.region_code
+  application_use   = "${var.application_use}"
+  tags = var.tags
+  # glue catalog database
+  glue_database_name = "connect_datalake_link"
+  glue_catalog_map = {}
+
+  add_linked_database = true
+  target_catalog_id = var.producer_catalog_id
+  target_database_name = var.source_database_name
+  
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = []
+  }
+}
+
+# Module to create data base for views in Lake formation
+module "glue_database_connect_datalake_views" {
+  source  = "app.terraform.io/SempraUtilities/seu-glue-crawler/aws"
+  version = "10.1.0"
+
+  company_code      = var.company_code
+  application_code  = var.application_code
+  environment_code  = var.environment_code
+  region_code       = var.region_code
+  application_use   = "${var.application_use}"
+  tags = var.tags
+
+  iam_role_arn  = data.aws_iam_session_context.current.issuer_arn
+  iam_role_name = data.aws_iam_session_context.current.issuer_name
+
+  glue_database_map = {
+    database1 = {
+      // job specific name here gets appended to standardized name
+      name = "connect_datalake_views"
+      optional_arguments = {
+        description = "This database stored all the views created on top of the Connect Data Lake Link used"
+      }
+    }
+  }
+}
--- terraform/lake_formation.tf
+++ terraform/lake_formation.tf
@@ -16,11 +16,11 @@ module "lake_formation" {
   application_use  = var.application_use
 
   # depends_on = [aws_glue_catalog_database.nice_glue_database, module.lakeformation_admin]
-  # depends_on = [module.lakeformation_admin]
+  depends_on = [module.lakeformation_admin, module.glue_data_catalog_connect_datalake, module.glue_database_connect_datalake_views ]
 
   # set_glue_data_catalog_permissions = true
   set_glue_data_catalog_permissions = false
-  use_lake_formation = false
+  use_lake_formation = true
 
   assign_iam_admin    = true
   iam_admin_role_arn  = data.aws_iam_session_context.current.issuer_arn
@@ -32,86 +32,26 @@ module "lake_formation" {
     element(split("/", var.admins_arn), length(split("/", var.admins_arn)) - 1),
     element(split("/", var.devs_arn), length(split("/", var.devs_arn)) - 1),
   ]
-}
 
-# Module to create database and tables using Lake formation
-
-# module "glue_data_catalog" {
-#   source  = "app.terraform.io/SempraUtilities/seu-glue-data-catalog/aws"
-#   version = "10.0.4"
-#   company_code      = var.company_code
-#   application_code  = var.application_code
-#   environment_code  = var.environment_code
-#   region_code       = var.region_code
-#   application_use   = "${var.application_use}"
-#   tags = var.tags
-#   # glue catalog database
-#   glue_database_name = "connect_database_link"
-#   glue_catalog_map = {}
-
-#   add_linked_database = true
-#   target_catalog_id = var.producer_catalog_id
-#   target_database_name = var.source_database_name
-  
-#   lifecycle {
-#     create_before_destroy = true
-#     ignore_changes        = []
-#   }
-# }
-
-resource "aws_glue_catalog_database" "glue_database_links" {
-  depends_on  = [module.lake_formation]
-  name        = "${var.source_database_name}_link"
-  target_database {
-    database_name = var.source_database_name
-    catalog_id    = var.producer_catalog_id
+  data_permission_map = {
+    permission1 = {
+      count         = length(var.quicksight_user_arns)
+      type          = "database"
+      # principal     = module.glue_data_catalog_connect_datalake.arn
+      principal     = element(var.quicksight_user_arns, count.index)
+      permissions   = ["DESCRIBE"]
+      database_name = module.glue_data_catalog_connect_datalake.glue_catalog_database_name
+    }
+    permission2 = {
+      count         = length(var.quicksight_user_arns)
+      type          = "table"
+      principal     = element(var.quicksight_user_arns,count.index)
+      permissions   = ["SELECT"]
+      principal     = element(var.quicksight_user_arns, floor(count.index / length(var.source_table_names)))
+      wildcard      = true
+    }
   }
 }
 
-resource "aws_lakeformation_permissions" "database" {
-  count       = length(var.quicksight_user_arns)
-  depends_on = [module.lakeformation_admin, resource.aws_glue_catalog_database.glue_database_links]
-  principal                     = element(var.quicksight_user_arns, count.index )
-  permissions                   = ["DESCRIBE"]
-  permissions_with_grant_option = ["DESCRIBE"]
-  database {
-    name = resource.aws_glue_catalog_database.glue_database_links.name
-    catalog_id = var.awsAccount
-  }
-}
-
-resource "aws_lakeformation_permissions" "table" {
-  depends_on = [module.lakeformation_admin, resource.aws_glue_catalog_database.glue_database_links, resource.aws_lakeformation_permissions.database]
-  count       = length(var.quicksight_user_arns) * length(var.source_table_names)
-  principal = element(var.quicksight_user_arns, floor(count.index / length(var.source_table_names)))
-  permissions = ["SELECT"]
-  permissions_with_grant_option = ["SELECT"]
-  table {
-    # database_name = resource.aws_glue_catalog_database.glue_database_links.name
-    database_name = var.source_database_name
-    name          = element(var.source_table_names, count.index)
-    # catalog_id = var.awsAccount
-  }
-}
-
-# Module to create database and tables using Lake formation
-
-# module "glue_data_catalog" {
-#   source  = "app.terraform.io/SempraUtilities/seu-glue-data-catalog/aws"
-#   version = "10.0.4"
-#   company_code      = var.company_code
-#   application_code  = var.application_code
-#   environment_code  = var.environment_code
-#   region_code       = var.region_code
-#   application_use   = "${var.application_use}"
-#   tags = var.tags
-#   # glue catalog database
-#   glue_database_name = "connect_database_test"
-#   glue_catalog_map = {}
-
-#   add_linked_database = true
-#   target_catalog_id = var.producer_catalog_id
-#   target_database_name = var.source_database_name
-# }
 
 
