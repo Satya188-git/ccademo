4580458c586a7567bd287d80a0e1f8f5bdd513a2
Saipraneeth Racharla <SRacharl@sdgecontractor.com>
2024-09-30 14:50:28 +0530

    Removed the artifacts that are no longer needed as per requirement. Stored them in feature/acrhive branch so that they can be reused incase they are needed in future.

--- terraform/glue_catalog.tf
+++ terraform/glue_catalog.tf
@@ -19,27 +19,27 @@ module "glue_data_catalog_connect_datalake" {
 }
 
 # Module to create data base for views in Lake formation
-module "glue_database_connect_datalake_views" {
-  source  = "app.terraform.io/SempraUtilities/seu-glue-crawler/aws"
-  version = "10.1.0"
+# module "glue_database_connect_datalake_views" {
+#   source  = "app.terraform.io/SempraUtilities/seu-glue-crawler/aws"
+#   version = "10.1.0"
 
-  company_code      = var.company_code
-  application_code  = var.application_code
-  environment_code  = var.environment_code
-  region_code       = var.region_code
-  application_use   = "${var.application_use}"
-  tags = var.tags
+#   company_code      = var.company_code
+#   application_code  = var.application_code
+#   environment_code  = var.environment_code
+#   region_code       = var.region_code
+#   application_use   = "${var.application_use}"
+#   tags = var.tags
 
-  iam_role_arn  = data.aws_iam_session_context.current.issuer_arn
-  iam_role_name = data.aws_iam_session_context.current.issuer_name
+#   iam_role_arn  = data.aws_iam_session_context.current.issuer_arn
+#   iam_role_name = data.aws_iam_session_context.current.issuer_name
 
-  glue_database_map = {
-    database1 = {
-      // job specific name here gets appended to standardized name
-      name = "connect_datalake_views"
-      optional_arguments = {
-        description = "This database stored all the views created on top of the Connect Data Lake Link used"
-      }
-    }
-  }
-}
+#   glue_database_map = {
+#     database1 = {
+#       // job specific name here gets appended to standardized name
+#       name = "connect_datalake_views"
+#       optional_arguments = {
+#         description = "This database stored all the views created on top of the Connect Data Lake Link used"
+#       }
+#     }
+#   }
+# }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -37,32 +37,11 @@ variable "tags" {
   description = "Common tags for all resources in this module."
 }
 
-variable "pandas_layer_arn" {
-  type = string
-  description = "AWSSDKPandas-Python311 Pandas Layer from AWS"
-}
-
-variable "code_artifact_bucket_name" {
-  description = "S3 bucket name for code artifacts"
-  type        = string
-}
 
 # variable "kms_key_id" {
 #   description = "Default KMS encryption KEY ID for the Secrets Manager"
 #   type        = string
 # }
-
-
-variable "nice_common_api_key" {
-  description = "Nice Common API Key from ADO"
-  type        = string
-}
-
-variable "nice_hist_queuestats_key" {
-  description = "Nice API Key hist queue stats from ADO"
-  type        = string
-}
-
 
 variable "admins_arn" {
   description = "This variable is for adding admin arn into lake formation policies"
