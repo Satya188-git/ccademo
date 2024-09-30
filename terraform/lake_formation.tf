b233344b46e4e5276d653367b762274aa08a7919
Saipraneeth Racharla <SRacharl@sdgecontractor.com>
2024-09-30 14:58:58 +0530

    Removed dependencies from glue

--- terraform/lake_formation.tf
+++ terraform/lake_formation.tf
@@ -11,7 +11,7 @@ module "lake_formation" {
   application_use  = var.application_use
 
   # depends_on = [aws_glue_catalog_database.nice_glue_database, module.lakeformation_admin]
-  depends_on = [module.lakeformation_admin, module.glue_data_catalog_connect_datalake, module.glue_database_connect_datalake_views ]
+  # depends_on = [module.lakeformation_admin, module.glue_data_catalog_connect_datalake, module.glue_database_connect_datalake_views ]
 
   # set_glue_data_catalog_permissions = true
   set_glue_data_catalog_permissions = true
