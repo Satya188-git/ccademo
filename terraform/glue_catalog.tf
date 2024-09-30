b157101556967201d09f7b9e94f6067f44c1e2a6
Saipraneeth Racharla <SRacharl@sdgecontractor.com>
2024-09-30 20:05:39 +0530

    replaced double quotes with single quotes

--- terraform/glue_catalog.tf
+++ terraform/glue_catalog.tf
@@ -82,7 +82,7 @@ module "glue_database_connect_datalake_views" {
         {
           ser_de_info_name                  = "my-stream"
           ser_de_info_serialization_library = "org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe"
-          ser_de_info_parameters            = tomap({'field.delim' = ',','skip.header.line.count' = '1' })
+          ser_de_info_parameters            = tomap({"field.delim" = ",","skip.header.line.count" = "1" })
         },
       ]
     }
