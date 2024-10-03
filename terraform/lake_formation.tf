c5ecd8d6dbe2cf035394ea993f7ca5bdb5527fcd
Saipraneeth Racharla <SRacharl@sdgecontractor.com>
2024-10-03 23:53:06 +0530

    Commented CIS Main

--- terraform/lake_formation.tf
+++ terraform/lake_formation.tf
@@ -31,7 +31,7 @@ module "lake_formation" {
   depends_on                      = [
                                       module.lakeformation_admin,
                                       module.glue_database_connect_datalake_views,
-                                      module.glue_data_catalog_cis_main,
+                                      # module.glue_data_catalog_cis_main,
                                       module.glue_data_catalog_connect_datalake,
                                     ]
 
