8c59528272e4c676794ce9fe2ee9f818dd56cf94
Saipraneeth Racharla <SRacharl@sdgecontractor.com>
2024-10-07 15:26:46 +0530

    added views

--- terraform/glue_catalog.tf
+++ terraform/glue_catalog.tf
@@ -21,22 +21,21 @@ module "glue_data_catalog_connect_datalake" {
 }
 
 # Module to create data base for views in Lake formation
-# module "glue_database_connect_datalake_views" {
-#   source                = "app.terraform.io/SempraUtilities/seu-glue-data-catalog/aws"
-#   version               = "10.0.4"
-#   company_code          = var.company_code
-#   application_code      = var.application_code
-#   environment_code      = var.environment_code
-#   region_code           = var.region_code
-#   application_use       = "${var.application_use}"
-#   tags                  = var.tags
+module "glue_database_connect_datalake_views" {
+  source                = "app.terraform.io/SempraUtilities/seu-glue-data-catalog/aws"
+  version               = "10.0.4"
+  company_code          = var.company_code
+  application_code      = var.application_code
+  environment_code      = var.environment_code
+  region_code           = var.region_code
+  application_use       = "${var.application_use}"
+  tags                  = var.tags
 
-#   # glue catalog database
-#   glue_database_name    = "connect-datalake-views"
-#   glue_catalog_map      = {}
-#   # depends_on = [ module.lake_formation ]
-
-# }
+  # glue catalog database
+  glue_database_name    = "connect-datalake-views"
+  glue_catalog_map      = {}
+  # depends_on = [ module.lake_formation ]
+}
 
 # Create a resource link from Connect API
 # module "glue_data_catalog_connect_api" {
--- terraform/lake_formation.tf
+++ terraform/lake_formation.tf
@@ -29,7 +29,8 @@ module "lake_formation" {
                                       ]
   depends_on                      = [
                                       module.lakeformation_admin,
-                                      module.glue_data_catalog_connect_datalake
+                                      module.glue_data_catalog_connect_datalake,
+                                      module.glue_database_connect_datalake_views
                                     ]
 
   # Adding DESCRIBE Permission on databases
