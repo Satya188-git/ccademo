9eb7dbe70c36c46556c4f8053580b48ff2ac890e
Saipraneeth Racharla <SRacharl@sdgecontractor.com>
2024-10-08 19:56:44 +0530

    removed description

--- terraform/glue_catalog.tf
+++ terraform/glue_catalog.tf
@@ -83,7 +83,6 @@ module "glue_database_connect_datalake_views" {
 
 resource "aws_glue_catalog_database" "glue_data_catalog_customer_cismain" {
   name         = "${var.company_code}-${var.application_code}-${var.environment_code}-${var.region_code}-${var.application_use}-customer-cismain-link"
-  description  = "Resource link for cis main"
   catalog_id   = var.awsAccount
 
   lifecycle {
