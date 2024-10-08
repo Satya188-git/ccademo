f00756a03444e94947de27e430f6bcb879accf4c
Saipraneeth Racharla <SRacharl@sdgecontractor.com>
2024-10-08 21:00:35 +0530

    added table & db permissions

--- terraform/lake_formation.tf
+++ terraform/lake_formation.tf
@@ -85,9 +85,14 @@ module "lake_formation" {
 resource "aws_lakeformation_permissions" "lf_cis_main" {
   principal   = "IAM_ALLOWED_PRINCIPALS"
   permissions = ["ALL", "DELETE", "DESCRIBE", "INSERT", "ALTER", "DROP"]
- 
+  
+  database {
+    name       = aws_glue_catalog_database.glue_data_catalog_customer_cismain.name
+    catalog_id = var.awsAccount
+  }
+
   table {
-    database_name       = aws_glue_catalog_database.glue_data_catalog_customer_cismain.name
-    wildcard            = true
+    database_name = aws_glue_catalog_database.glue_data_catalog_customer_cismain.name
+    name = "contract_account_attributes"
   }
 }
