cba8fd6f43f90bd4eb74fae04f0502285dec49d2
Saipraneeth Racharla <SRacharl@sdgecontractor.com>
2024-10-09 01:56:12 +0530

    adding table permissions for cis link

--- terraform/lake_formation.tf
+++ terraform/lake_formation.tf
@@ -82,25 +82,16 @@ module "lake_formation" {
 }
 }
 
-# resource "aws_lakeformation_permissions" "lf_cis_main_db" {
-#   principal   = "arn:aws:quicksight:us-west-2:442426866507:user/default/AWSReservedSSO_sdge-dcctr-dev-admin_f4611a12900c932f/SRacharl@sdgecontractor.com"
-#   permissions = ["DESCRIBE"]
-  
-#   database {
-#     name       = "sdge_dev_wus2_customer_cismain"
-#     catalog_id = var.connect_api_catalog_id
-#   }
-# }
 
-# resource "aws_lakeformation_permissions" "lf_cis_main_table" {
-#   depends_on = [ aws_lakeformation_permissions.lf_cis_main_db ]
-#   principal   = "arn:aws:quicksight:us-west-2:442426866507:user/default/AWSReservedSSO_sdge-dcctr-dev-admin_f4611a12900c932f/SRacharl@sdgecontractor.com"
-#   permissions = ["SELECT"]
+
+resource "aws_lakeformation_permissions" "lf_cis_main_table" {
+  depends_on = [ aws_lakeformation_permissions.lf_cis_main_db ]
+  principal   = "arn:aws:quicksight:us-west-2:442426866507:user/default/AWSReservedSSO_sdge-dcctr-dev-admin_f4611a12900c932f/SRacharl@sdgecontractor.com"
+  permissions = ["SELECT"]
   
-#   table {
-#     catalog_id   = var.connect_api_catalog_id
-#     database_name = "sdge_dev_wus2_customer_cismain"
-#     # name = "contract_account_attributes"
-#     wildcard = true
-#   }
-# }
+  table {
+    database_name = module.glue_data_catalog_connect_datalake.glue_catalog_database_name
+    # name = "contract_account_attributes"
+    wildcard = true
+  }
+}
