2940c284807e863ad26923738e01be959d1efda3
Saipraneeth Racharla <SRacharl@sdgecontractor.com>
2024-10-08 22:13:26 +0530

    added permissions for db and table

--- terraform/lake_formation.tf
+++ terraform/lake_formation.tf
@@ -81,10 +81,20 @@ module "lake_formation" {
 # }
 }
 
-
-resource "aws_lakeformation_permissions" "lf_cis_main" {
+resource "aws_lakeformation_permissions" "lf_cis_main_db" {
   principal   = "arn:aws:quicksight:us-west-2:442426866507:user/default/AWSReservedSSO_sdge-dcctr-dev-admin_f4611a12900c932f/SRacharl@sdgecontractor.com"
-  permissions = ["DESCRIBE", "SELECT"]
+  permissions = ["DESCRIBE"]
+  
+  database {
+    name       = "sdge_dev_wus2_customer_cismain"
+    catalog_id = var.connect_api_catalog_id
+  }
+}
+
+resource "aws_lakeformation_permissions" "lf_cis_main_table" {
+  depends_on = [ aws_lakeformation_permissions.lf_cis_main_db ]
+  principal   = "arn:aws:quicksight:us-west-2:442426866507:user/default/AWSReservedSSO_sdge-dcctr-dev-admin_f4611a12900c932f/SRacharl@sdgecontractor.com"
+  permissions = ["SELECT"]
   
   table {
     catalog_id   = var.connect_api_catalog_id
