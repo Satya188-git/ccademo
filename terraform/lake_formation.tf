68366bb14908300e57a24c68558efb62141c30e3
Saipraneeth Racharla <SRacharl@sdgecontractor.com>
2024-10-14 22:36:15 +0530

    updated qs permissions for views database in QS Deploy

--- terraform/lake_formation.tf
+++ terraform/lake_formation.tf
@@ -82,7 +82,21 @@ module "lake_formation" {
       permissions   = ["SELECT"]
       database_name = aws_glue_catalog_database.glue_data_catalog_connect_datalake.name
       wildcard      = true
+    },
+    permission8     = {
+      type          = "database"
+      principal     = var.qs_arn
+      permissions   = ["DESCRIBE"]
+      database_name = aws_glue_catalog_database.glue_database_connect_datalake_views.name
+    },
+    permission9     = {
+      type          = "table"
+      principal     = var.qs_arn
+      permissions   = ["SELECT"]
+      database_name = aws_glue_catalog_database.glue_database_connect_datalake_views.name
+      wildcard      = true
     }
+
   }
 }
 
