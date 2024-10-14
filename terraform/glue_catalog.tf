7c141db50bbe2908104fd5b5950b0adf1c066dd7
Saipraneeth Racharla <SRacharl@sdgecontractor.com>
2024-10-14 21:06:58 +0530

    added all connect tables in a single go

--- terraform/glue_catalog.tf
+++ terraform/glue_catalog.tf
@@ -71,28 +71,17 @@ resource "aws_glue_catalog_database" "glue_data_catalog_connect_datalake" {
   }
 }
 
-resource "aws_glue_catalog_table" "shared_connect_record_link" {
-  name          = "contact_record"  # Name for the resource link table
+resource "aws_glue_catalog_table" "shared_connect_link" {
+  for_each      = { for table in var.source_table_names : table.name => table }  # Create a map from the list for iteration
+  
+  name          = each.value.name
   database_name = aws_glue_catalog_database.glue_data_catalog_connect_datalake.name
- 
-  table_type = "LINK"
- 
+  table_type    = "LINK"
+
   target_table {
-    catalog_id   = var.producer_catalog_id  # Replace with the AWS account ID where the original table resides
-    database_name = var.source_database_name  # The original Glue database name in the other account
-    name          = "contact_record"  # The original table name in the shared Glue database
+    catalog_id     = var.producer_catalog_id
+    database_name  = each.value.source_database_name
+    name           = each.value.name
   }
 }
 
-resource "aws_glue_catalog_table" "shared_connect_cont_static_link" {
-  name          = "contact_statistic_record"  # Name for the resource link table
-  database_name = aws_glue_catalog_database.glue_data_catalog_connect_datalake.name
- 
-  table_type = "LINK"
- 
-  target_table {
-    catalog_id   = var.producer_catalog_id  # Replace with the AWS account ID where the original table resides
-    database_name = var.source_database_name  # The original Glue database name in the other account
-    name          = "contact_statistic_record"  # The original table name in the shared Glue database
-  }
-}
