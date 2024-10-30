35da90971742d063d6a99978cea3968b155a1df9
Saipraneeth Racharla <SRacharl@sdgecontractor.com>
2024-10-30 16:09:17 +0530

    added qs federated reader role, policy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -20,3 +20,50 @@ resource "aws_iam_role_policy" "lf_policy" {
     {}
   )
 }
+
+module "qs_reader" {
+  source            = "app.terraform.io/SempraUtilities/seu-iam-role/aws"
+  version           = "10.0.2"
+  company_code      = var.company_code
+  application_code  = var.application_code
+  environment_code  = var.environment_code
+  region_code       = var.region_code
+  application_use   = "${var.application_use}-qs-reader"
+  description       = "QuickSight-Reader-Role"
+  service_resources = ["*"]
+  tags              = var.tags
+  additional_policy_statements = [
+    {
+      "Effect" : "Allow",
+      "Principal" : {
+        "Federated" : "arn:aws:iam::${var.awsAccount}:saml-provider/AzureActiveDirectory"
+      },
+      "Action" : "sts:AssumeRoleWithSAML",
+      "Condition" : {
+        "StringEquals" : {
+          "SAML:aud" : "https://signin.aws.amazon.com/saml"
+        }
+      }
+    }
+  ]
+}
+
+resource "aws_iam_policy" "qs_reader_policy" {
+  name        = "QuickSight-Federated-Reader"
+  description = "A policy for QuickSight-Reader-Role"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "quicksight:CreateReader"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "qs_reader_policy_attachment" {
+  role       = module.qs_reader.name
+  policy_arn = aws_iam_policy.qs_reader_policy.arn
+}
