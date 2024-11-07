8b65002028c414adb6e43518f95581d65180bead
Saipraneeth Racharla <SRacharl@sdgecontractor.com>
2024-11-07 23:47:57 +0530

    commented kms in sns

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -105,10 +105,10 @@ variable "code_artifact_bucket_name" {
   type        = string
 }
 
-variable "sns_topic_key_id" {
-  type        = string
-  description = "sns_topic_key_id for encryption"
-}
+# variable "sns_topic_key_id" {
+#   type        = string
+#   description = "sns_topic_key_id for encryption"
+# }
 
 variable "email_value_map" {
     description = "Map of environment values"
