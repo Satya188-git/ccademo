30cf53f386ea628eca608979f38b7209beea860c
Saipraneeth Racharla <SRacharl@sdgecontractor.com>
2024-11-07 20:16:50 +0530

    lambda for conatinment alerts

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -21,6 +21,37 @@ resource "aws_iam_role_policy" "lf_policy" {
   )
 }
 
+module "lambda_role" {
+  source  = "app.terraform.io/SempraUtilities/seu-iam-role/aws"
+  version = "10.0.2"
+
+  company_code      = var.company_code
+  application_code  = var.application_code
+  environment_code  = var.environment_code
+  region_code       = var.region_code
+  application_use   = "${var.application_use}-containment-alerts"
+  description       = "This is a lambda role to query data from athena and send alerts"
+  service_resources = ["lambda.amazonaws.com"]
+  tags              = var.tags
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name   = "${var.company_code}-${var.application_code}-${var.environment_code}-${var.region_code}-${var.application_use}-containment-alerts-policy"
+  role   = module.lambda_role.name
+  policy = templatefile(
+    "${path.module}/iampolicies/policy-iam-lambda-assume-role.tmpl",
+    {
+      region = "us-west-2",
+      region_code = var.region_code
+      account = var.awsAccount,
+      company_code = var.company_code,
+      application_code = var.application_code,
+      environment_code = var.environment_code,
+      application_use = "${var.application_use}-containment-alerts"
+    }
+  )
+}
+
 resource "aws_iam_role" "qs_admin" {
   name        = "${var.company_code}-${var.application_code}-${var.environment_code}-${var.region_code}-iam-role-${var.application_use}-qs-admin"
   description = "QuickSight-Admin-Role"
