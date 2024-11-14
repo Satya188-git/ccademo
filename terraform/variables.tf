3f61a90e09c26386fdc089edf9e6ccff8c65b69c
Souvik Nayak <souvik.nath.nayak@accenture.com>
2024-11-14 20:18:49 +0530

    commenting out code in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -130,10 +130,10 @@ variable "ado_role_name" {
   description = "ADO role name to assume"
 }
 
-variable "sdge_domain_identity_verification_record" {
-  type        = string
-  description = "sdge_domain_identity_verification_record"
-}
+#variable "sdge_domain_identity_verification_record" {
+#  type        = string
+#  description = "sdge_domain_identity_verification_record"
+#}
 
 #variable "r53_zone_name" {
 #  type        = string
