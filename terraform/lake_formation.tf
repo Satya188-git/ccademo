2e094efab2f91bf757ec71c72e4136e9c932f455
Saipraneeth Racharla <SRacharl@sdgecontractor.com>
2024-11-19 21:10:01 +0530

    updated the lf permissions

--- terraform/lake_formation.tf
+++ terraform/lake_formation.tf
@@ -193,42 +193,14 @@ module "lake_formation" {
     #   database_name = aws_glue_catalog_database.glue_data_catalog_connect_datalake.name
     #   wildcard      = true
     # },
-    permission23 = {
-      type          = "database"
-      principal     = "IAM_ALLOWED_PRINCIPALS"
-      permissions   = ["DESCRIBE"]
-      database_name = aws_glue_catalog_database.glue_data_catalog_customer_connectchatbot.name
-    },
+    # permission23 = {
+    #   type          = "database"
+    #   principal     = "IAM_ALLOWED_PRINCIPALS"
+    #   permissions   = ["DESCRIBE"]
+    #   database_name = aws_glue_catalog_database.glue_data_catalog_customer_connectchatbot.name
+    # },
   }
 }
-
-# List of table names (Manually specify the table names here)
-locals {
-  table_names = [
-    "sdge_connect_aws_lambda_sdge_dhepk_sbx_wus2_lambda_einstein_lex_bot_faq",
-    "sdge_connect_aws_connect_sdge_dhepk_sbx_wus2_einstein_connect",
-    "sdge_connect_aws_lambda_sdge_dhepk_sbx_wus2_lambda_einstein_lex_bot_faq_async"
-  ]
-}
-
-resource "aws_lakeformation_permissions" "connect_chatbot_permissions" {
-  depends_on  = [module.lakeformation_admin, aws_glue_catalog_database.glue_data_catalog_customer_connectchatbot]
-  principal   = "IAM_ALLOWED_PRINCIPALS"
-  permissions = ["SELECT", "ALTER"]
-
-  for_each = toset(local.table_names) # Iterate over the table names
-
-  table {
-    database_name = aws_glue_catalog_database.glue_database_connect_datalake_views.name
-    catalog_id    = var.awsAccount
-    name          = each.value # Use the current table name from the loop
-  }
-
-  lifecycle {
-    ignore_changes = all
-  }
-}
-
 
 
 resource "aws_lakeformation_permissions" "ivr_call_events_permissions" {
