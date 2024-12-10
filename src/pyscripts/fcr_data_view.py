f6028724a0e036793ea7191c5cba70b1f36c179c
Souvik Nayak <souvik.nath.nayak@accenture.com>
2024-12-10 18:33:19 +0530

    Pushing FCR View changes

--- src/pyscripts/fcr_data_view.py
+++ src/pyscripts/fcr_data_view.py
@@ -17,22 +17,23 @@ view_name = "fcr_data_view"
 print("Executing the view: ")
 start_query_response = client.start_query_execution(
     QueryString = f"""CREATE OR REPLACE VIEW {view_name} AS 
-        SELECT 
+    SELECT 
     customer_endpoint_address,
     a.contact_id,
     queue_enqueue_timestamp,
     agent_username,
     queue_name AS requested_call_type,
-    agent_connected_to_agent_timestamp,
     CASE 
         WHEN actual_call_type IS NULL THEN queue_name 
         ELSE actual_call_type 
     END AS actual_call_type,
-    COALESCE(
-        DATE_PARSE(SUBSTR(CAST(agent_connected_to_agent_timestamp AS VARCHAR), 1, 19), '%Y-%m-%d %H:%i:%s'),
-        DATE_PARSE(SUBSTR(CAST(initiation_timestamp AS VARCHAR), 1, 19), '%Y-%m-%d %H:%i:%s')
-    ) AS initiation_timestamp,
+    
+    DATE_PARSE(SUBSTR(CAST(initiation_timestamp AS VARCHAR), 1, 19), '%Y-%m-%d %H:%i:%s') AS initiation_timestamp_original,
+    
+    DATE_PARSE(SUBSTR(CAST(agent_connected_to_agent_timestamp AS VARCHAR), 1, 19), '%Y-%m-%d %H:%i:%s') AS initiation_timestamp,
+    
     DATE_PARSE(SUBSTR(CAST(disconnect_timestamp AS VARCHAR), 1, 19), '%Y-%m-%d %H:%i:%s') AS disconnect_timestamp,
+    
     CASE 
         WHEN LEAD(agent_connected_to_agent_timestamp) 
              OVER (PARTITION BY customer_endpoint_address, queue_name ORDER BY agent_connected_to_agent_timestamp) IS NULL
@@ -44,34 +45,37 @@ start_query_response = client.start_query_execution(
             OVER (PARTITION BY customer_endpoint_address, queue_name ORDER BY agent_connected_to_agent_timestamp)
         ) 
     END AS time_difference_seconds,
-    SUBSTR(CAST(initiation_timestamp AS VARCHAR), 1, 4) AS year
-        FROM 
-            \"sdge-dcctr-{env}-wus2-ccc-analytics-connect-datalake-link\".\"contact_record\" AS a
-        LEFT JOIN
-            (
-                SELECT 
-                    contact_id, 
-                    actual_call_type 
-                FROM (
-                    SELECT 
-                        initial_contact_id AS contact_id,
-                        queue_name AS actual_call_type,
-                        RANK() OVER (PARTITION BY initial_contact_id ORDER BY disconnect_timestamp DESC) AS rank
-                    FROM 
-                        \"sdge-dcctr-{env}-wus2-ccc-analytics-connect-datalake-link\".\"contact_record\"
-                    WHERE 
-                        initial_contact_id IS NOT NULL
-                ) 
-                WHERE rank = 1
-            ) AS b
-        ON 
-            a.contact_id = b.contact_id
-        WHERE 
+    
+    SUBSTR(CAST(agent_connected_to_agent_timestamp AS VARCHAR), 1, 4) AS year
+    
+FROM 
+    \"dev_connectdatalake\".\"dev_contact_record\" AS a
+LEFT JOIN
+    (
+        SELECT 
+            contact_id, 
+            actual_call_type 
+        FROM (
+            SELECT 
+                initial_contact_id AS contact_id,
+                queue_name AS actual_call_type,
+                RANK() OVER (PARTITION BY initial_contact_id ORDER BY disconnect_timestamp DESC) AS rank
+            FROM 
+                \"dev_connectdatalake\".\"dev_contact_record\"
+            WHERE 
+                initial_contact_id IS NOT NULL
+        ) 
+        WHERE rank = 1
+    ) AS b
+ON 
+    a.contact_id = b.contact_id
+WHERE 
     queue_name IS NOT NULL
+    AND agent_interaction_duration_ms > 0
     AND upper(channel) = 'VOICE'
     AND upper(initiation_method) = 'INBOUND';""",
     QueryExecutionContext={
-        'Database': f"sdge-dcctr-{env}-wus2-ccc-analytics-connect-datalake-views",
+        'Database': f"dev_connectdatalake",
         'Catalog': 'awsdatacatalog'
     },
     ResultConfiguration={
@@ -79,6 +83,7 @@ start_query_response = client.start_query_execution(
     },
     WorkGroup='primary'
 )
+
 
  
 print(f"The status of the execution using API call is : {start_query_response['ResponseMetadata']['HTTPStatusCode']}")
