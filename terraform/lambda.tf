b6f3aa92fa410e4018c51871c1733e4c54d3bfad
Saipraneeth Racharla <SRacharl@sdgecontractor.com>
2025-01-21 22:47:26 +0530

    updated the cra lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -19,7 +19,7 @@ module "containment_alerts_lambda" {
   lambda_role                   = module.lambda_role.name
   tags                          = var.tags
   layers                        = [var.pandas_layer_arn]
-  publish                       = true
+  publish                       = false
   attach_cloudwatch_logs_policy = false
   create                        = true
   create_function               = true
