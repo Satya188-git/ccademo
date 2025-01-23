89347578f16a4352b2f547069a2aed5fed26c037
Saipraneeth Racharla <SRacharl@sdgecontractor.com>
2025-01-23 19:16:34 +0530

    lambda update fix

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,41 +1,4 @@
 # # Lambda function to send Containment Rate Alerts
-# module "containment_alerts_lambda" {
-
-#   depends_on = [module.lambda_role]
-#   source     = "app.terraform.io/SempraUtilities/seu-lambda/aws"
-#   version    = "10.0.0"
-
-#   company_code                  = var.company_code
-#   application_code              = var.application_code
-#   environment_code              = var.environment_code
-#   region_code                   = var.region_code
-#   application_use               = "${var.application_use}-containment-alerts"
-#   description                   = "Lambda function for Containment Rate Alerts"
-#   handler                       = "run.lambda_handler"
-#   publish                       = false
-#   runtime                       = "python3.11"
-#   memory_size                   = "1024"
-#   timeout                       = "300"
-#   architectures                 = ["x86_64"]
-#   lambda_role                   = module.lambda_role.name
-#   tags                          = var.tags
-#   layers                        = [var.pandas_layer_arn]
-#   environment_variables = {
-#     env = "${var.environment_code}"
-#   }
-#   s3_existing_package = {
-#     bucket = "${var.company_code}-${var.application_code}-${var.environment_code}-${var.region_code}-s3-artifacts"
-#     key    = "lambda/containment_alerts.zip"
-    
-#   }
-# }
-
-data "archive_file" "lambda" {
-  type        = "zip"
-  source_file = "src/lambda/containment_alerts/run.py"
-  output_path = "containment_alerts.zip"
-}
-
 module "containment_alerts_lambda" {
 
   depends_on = [module.lambda_role]
@@ -60,6 +23,43 @@ module "containment_alerts_lambda" {
   environment_variables = {
     env = "${var.environment_code}"
   }
-  local_existing_package = data.archive_file.lambda.output_path
-  # source_code_hash = data.archive_file.lambda.output_base64sha256
+  s3_existing_package = {
+    bucket = "${var.company_code}-${var.application_code}-${var.environment_code}-${var.region_code}-s3-artifacts"
+    key    = "lambda/containment_alerts.zip"
+    
+  }
 }
+
+# data "archive_file" "lambda" {
+#   type        = "zip"
+#   source_file = "src/lambda/containment_alerts/run.py"
+#   output_path = "containment_alerts.zip"
+# }
+
+# module "containment_alerts_lambda" {
+
+#   depends_on = [module.lambda_role]
+#   source     = "app.terraform.io/SempraUtilities/seu-lambda/aws"
+#   version    = "10.0.0"
+
+#   company_code                  = var.company_code
+#   application_code              = var.application_code
+#   environment_code              = var.environment_code
+#   region_code                   = var.region_code
+#   application_use               = "${var.application_use}-containment-alerts"
+#   description                   = "Lambda function for Containment Rate Alerts"
+#   handler                       = "run.lambda_handler"
+#   publish                       = false
+#   runtime                       = "python3.11"
+#   memory_size                   = "1024"
+#   timeout                       = "300"
+#   architectures                 = ["x86_64"]
+#   lambda_role                   = module.lambda_role.name
+#   tags                          = var.tags
+#   layers                        = [var.pandas_layer_arn]
+#   environment_variables = {
+#     env = "${var.environment_code}"
+#   }
+#   local_existing_package = data.archive_file.lambda.output_path
+#   # source_code_hash = data.archive_file.lambda.output_base64sha256
+# }
